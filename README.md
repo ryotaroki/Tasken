02ff844cc41a3c2b7643f0325773e7eaacb39589
ryota <giants.hara.love@gmail.com>
2022-11-11 10:50:42 +0900

    Taskのview作成、taskContorollerにindexの記述

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Laravel 復習用。
+Laravel8のRoute
+・名前空間が削除されたのでweb.phpでuseにより使うコントローラーの記述をする。
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\TaskController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,6 +17,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('tasks', function() {
-    return view('tasks', 'TaskController@index');
-});
+Route::resource("/tasks", TaskController::class);
